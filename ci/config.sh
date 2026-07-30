bbb0701d384954986908bfca54729cc754c1a1a6
Nick Altmann <nick@altmann.pro>
2026-07-30 13:51:31 +0200

    Increase keep_days to 90

--- ci/config.sh
+++ ci/config.sh
@@ -9,7 +9,7 @@ BUILD_WHAT="${BUILD_WHAT:-4.0 3.6 devel}"
 BUILD_FOR="${BUILD_FOR:-ubuntu-resolute/amd64 ubuntu-noble/amd64 ubuntu-jammy/amd64 \
                         debian-trixie/amd64 debian-bookworm/amd64 debian-bullseye/amd64 \
                         el-9/x86_64 st-9/x86_64}"
-KEEP_DAYS=30
+KEEP_DAYS=90
 
 BUILD_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)/.opensips-build"
 SOURCE_DIR="${BUILD_ROOT}/SOURCES"
